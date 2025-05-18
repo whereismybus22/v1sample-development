0778ef35cb948c038c99dfe16986353177700364
akshara799 <aksharareddy799@gmail.com>
2025-05-18 19:46:31 +0530

    removed component

--- js/topbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('topbar.html')
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('topbar-container').innerHTML = html;
-    })
-    .catch(err => console.error('Failed to load topbar:', err));
-});
