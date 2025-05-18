b192ef5cc87b5e2e86f689aad129e39a4af67135
akshara799 <aksharareddy799@gmail.com>
2025-05-19 01:46:15 +0530

    added youtube video in tutorial page

--- js/tutorialskip.js
+++ js/tutorialskip.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const videoTrigger = document.getElementById('videoTrigger');
+  const openVideoBtn = document.getElementById('openVideoBtn');
+  const youtubeLink = "https://youtu.be/sL8-vrfTR7k?si=WEGGghzL8HkHoyoX";
+
+  function openYouTubeVideo() {
+    localStorage.setItem("isTutorialSeen", JSON.stringify(true));
+    window.open(youtubeLink, '_blank');
+  }
+
+  if (videoTrigger) {
+    videoTrigger.addEventListener('click', openYouTubeVideo);
+  }
+
+  if (openVideoBtn) {
+    openVideoBtn.addEventListener('click', openYouTubeVideo);
+  }
+});
+
+window.addEventListener('focus', () => {
+  if (localStorage.getItem('isTutorialSeen')) {
+    window.location = '/index.html';
+  }
+});
 document.getElementById('skiptobusesButton').addEventListener('click', function() {
     window.location.href = '../pages/buses.html'; 
 });
