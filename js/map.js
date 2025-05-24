dc3591984ba5058d28cb5f5a501db15ae38a6ae9
akshara799 <aksharareddy799@gmail.com>
2025-05-24 19:52:19 +0530

    loading of map page in animation

--- js/map.js
+++ js/map.js
@@ -1,3 +1,19 @@
+
+let startTime = Date.now();
+let timerInterval;
+let elapsedTime = 0;
+function updateTimer() {
+  elapsedTime++;
+  document.getElementById('loadingTime').innerText = `${elapsedTime} seconds`;
+}
+
+timerInterval = setInterval(updateTimer, 1000);
+
+setTimeout(() => {
+  clearInterval(timerInterval); 
+  document.getElementById('splashScreen').style.display = 'none';
+  document.getElementById('trackerContent').style.display = 'block';
+}, 5000); 
 window.addEventListener("DOMContentLoaded", () => {
   const routeElement = document.getElementById("routeNumber");
   const routeValue = localStorage.getItem("defaultBusRoute");
