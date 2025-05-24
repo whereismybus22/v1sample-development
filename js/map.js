c778b1902f74b97b6e33c6919bb9cc1fb6422c79
akshara799 <aksharareddy799@gmail.com>
2025-05-24 20:24:44 +0530

    added mapcontainer with timer

--- js/map.js
+++ js/map.js
@@ -1,29 +1,27 @@
-
-let startTime = Date.now();
-let timerInterval;
-let elapsedTime = 0;
-function updateTimer() {
-  elapsedTime++;
-  document.getElementById('loadingTime').innerText = `${elapsedTime} seconds`;
-}
-
-timerInterval = setInterval(updateTimer, 1000);
-
-setTimeout(() => {
-  clearInterval(timerInterval); 
-  document.getElementById('splashScreen').style.display = 'none';
-  document.getElementById('trackerContent').style.display = 'block';
-}, 5000); 
 window.addEventListener("DOMContentLoaded", () => {
-  const routeElement = document.getElementById("routeNumber");
-  const routeValue = localStorage.getItem("defaultBusRoute");
+      startTimer();
+      setTimeout(() => {
+        document.getElementById("splashScreen").style.display = "none";
+        document.getElementById("trackerContent").style.display = "block";
+        document.getElementById("mapContainer").style.display = "flex";
+      }, 5000);
+    });
+ let timer = 0;
+    let timerInterval;
 
-  if (routeValue) {
-    const routeNumberOnly = routeValue.match(/\d+/);
-    if (routeNumberOnly) {
-      routeElement.textContent = `Route ${routeNumberOnly[0]}`;
-    } else {
-      routeElement.textContent = "Route --"; 
+        function startTimer() {
+      timerInterval = setInterval(() => {
+        timer++;
+        document.getElementById("timerDisplay").textContent = `Timer: ${timer}`;
+      }, 1000); 
     }
-  } 
-});
+    const routeElement = document.getElementById("routeNumber");
+    const routeValue = localStorage.getItem("defaultBusRoute");
+    if (routeValue) {
+      const routeNumberOnly = routeValue.match(/\d+/);
+      if (routeNumberOnly) {
+        routeElement.textContent = `Route ${routeNumberOnly[0]}`;
+      } else {
+        routeElement.textContent = "Route --"; 
+      }
+    } 
