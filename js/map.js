29dcae5d3e0b6cbd50e03fcdc84915ed6bb4d5a1
akshara799 <aksharareddy799@gmail.com>
2025-05-24 20:51:16 +0530

    added logic for animation

--- js/map.js
+++ js/map.js
@@ -1,27 +1,33 @@
 window.addEventListener("DOMContentLoaded", () => {
-      startTimer();
-      setTimeout(() => {
-        document.getElementById("splashScreen").style.display = "none";
-        document.getElementById("trackerContent").style.display = "block";
-        document.getElementById("mapContainer").style.display = "flex";
-      }, 4000);
-    });
- let timer = 0;
-    let timerInterval;
+  startTimer();
 
-        function startTimer() {
-      timerInterval = setInterval(() => {
-        timer++;
-        document.getElementById("timerDisplay").textContent = `Timer: ${timer}`;
-      }, 1000); 
-    }
-    const routeElement = document.getElementById("routeNumber");
-    const routeValue = localStorage.getItem("defaultBusRoute");
-    if (routeValue) {
-      const routeNumberOnly = routeValue.match(/\d+/);
-      if (routeNumberOnly) {
-        routeElement.textContent = `Route ${routeNumberOnly[0]}`;
-      } else {
-        routeElement.textContent = "Route --"; 
-      }
-    } 
+  const lottiePlayer = document.querySelector('dotlottie-player');
+
+  lottiePlayer.addEventListener('complete', function() {
+    document.getElementById("splashScreen").style.display = "none";
+    document.getElementById("trackerContent").style.display = "block";
+    document.getElementById("mapContainer").style.display = "flex";
+  });
+});
+
+let timer = 0;
+let timerInterval;
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timer++;
+    document.getElementById("timerDisplay").textContent = `Timer: ${timer}`;
+  }, 1000); 
+}
+
+const routeElement = document.getElementById("routeNumber");
+const routeValue = localStorage.getItem("defaultBusRoute");
+
+if (routeValue) {
+  const routeNumberOnly = routeValue.match(/\d+/);
+  if (routeNumberOnly) {
+    routeElement.textContent = `Route ${routeNumberOnly[0]}`;
+  } else {
+    routeElement.textContent = "Route --"; 
+  }
+} 
